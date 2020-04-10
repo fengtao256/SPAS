bb982a96b286fdb21c5f21969114e265aa3b8ceb
fengtao256 <fengtao256@qq.com>
2020-04-10 13:35:41 +0800

    首次提交SPAS程序架子

---
+++ src/main/java/com/lzl/spas/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lzl.spas;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpasApplication.class);
+    }
+
+}
