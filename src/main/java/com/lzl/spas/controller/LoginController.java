69a0c3f812eeba55a722786354ffec841a7ed596
fengtao256 <fengtao256@qq.com>
2020-04-16 14:04:12 +0800

    编写登陆页面

--- src/main/java/com/lzl/spas/controller/LoginController.java
+++ src/main/java/com/lzl/spas/controller/LoginController.java
@@ -3,15 +3,13 @@ package com.lzl.spas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWord {
+public class LoginController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/login.action")
     public String hello(Model model){
-        model.addAttribute("welcome","刘卓林");
-        System.out.println("测试返回model===="+model.getAttribute("welcome"));
+        model.addAttribute("titleHead","欢迎登陆学生成绩分析系统");
         return "login" ;
     }
 }
